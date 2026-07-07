defcf0e8493dd29824958ed207ae7ea295e997a2
meowxd <68319010043@loeitech.ac.th>
2026-07-07 12:08:51 +0700

    feat: add docker

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const sequelize = require('./db');
 const Computer = require('./models/Computer');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
